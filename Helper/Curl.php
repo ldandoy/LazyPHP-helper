df36d3c75b51556ccbbd4f5c35d9eff3525becd5
loic dandoy <ldandoy@overconsulting.com>
2018-10-17 16:21:21 +0200

    Correction pour accepter le json

--- Helper/Curl.php
+++ Helper/Curl.php
@@ -29,6 +29,8 @@ class Curl
 	{
 		$curl = \curl_init();
 
+		$data_string = json_encode($data);
+
 		\curl_setopt($curl, CURLOPT_URL, $url);
 		\curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
 		\curl_setopt($curl, CURLOPT_FOLLOWLOCATION, true);
@@ -36,7 +38,12 @@ class Curl
 		\curl_setopt($curl, CURLOPT_CUSTOMREQUEST, 'POST');
 		\curl_setopt($curl, CURLOPT_POST, true);
 		\curl_setopt($curl, CURLOPT_POSTREDIR, 3);
-		\curl_setopt($curl, CURLOPT_POSTFIELDS, $data);
+		\curl_setopt($curl, CURLOPT_POSTFIELDS, $data_string);                                                                    
+		\curl_setopt($curl, CURLOPT_HTTPHEADER, array(                                                                          
+			'Content-Type: application/json',
+			'Accept: application/json',
+			'Content-Length: ' . strlen($data_string))
+		);
 	
 		$res = \curl_exec($curl);
 
